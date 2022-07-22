48431a978575561912805de0bf13a367310e7430
William Luck <luck.william.h@gmail.com>
2022-07-22 12:41:10 -0500

    commit tree bacbcb9bc25e3cbd2ba2ff6ad03e68404db8c609

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,25 +2,18 @@ import React, { useEffect, useState } from "react";
 import About from "./About";
 import ArticleList from "./ArticleList";
 
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
+
 function HomePage() {
   // fetch data for posts
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [posts, setPosts] = useState([]);
+  
 
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch("http://localhost:4000/posts")
-      .then((r) => r.json())
-      .then((posts) => {
-        setPosts(posts);
-        setIsLoaded(true);
-      });
-  }, []);
+  const { data: posts, isLoaded } = useQuery('http://localhost:4000/posts')
+  // Reanmes the variable to posts when destructuring
 
   // set the document title
-  useEffect(() => {
-    document.title = "Underreacted | Home";
-  }, []);
+  useDocumentTitle('Underreacted | Home')
 
   return (
     <>
--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 
+
+// Custom hooks should start with 'use' to signal to react that is a custom hook
+
 function useDocumentTitle(pageTitle) {
   useEffect(() => {
     document.title = pageTitle;
